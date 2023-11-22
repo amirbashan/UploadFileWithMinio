35b7d16d3b7539b18325136bd36c08c31cc8a332
netanelsh0 <netanelsh0@gmail.com>
2023-11-22 16:25:29 +0200

    Clean up

---
+++ front/src/App.tsx
@@ -0,0 +1,142 @@
+import React, { ChangeEvent, useEffect, useState } from "react";
+import "./App.css";
+import { Button, CircularProgress, IconButton, styled } from "@mui/material";
+import axios from "axios";
+import CollectionsBookmarkIcon from "@mui/icons-material/CollectionsBookmark";
+import AllImagesModal from "./AllImagesModal";
+
+const StyledIconButton = styled(IconButton)({
+  "&:focus": {
+    outline: "none",
+  },
+});
+
+function App() {
+  const [uploadedFile, setUploadedFile] = useState<string | null>(null);
+  const [imagesUrl, setImagesUrl] = useState<Array<string>>();
+  const [isClickShowMore, setIsClickShowMore] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const fetchImages = async () => {
+      const { data } = await axios.get("http://localhost:3002/getImages");
+      setImagesUrl(data);
+    };
+    fetchImages();
+  }, [setUploadedFile, isClickShowMore]);
+  const handleOnChane = async (e: ChangeEvent<HTMLInputElement>) => {
+    try {
+      if (e?.target?.files) {
+        setIsLoading(true);
+        const file = e.target.files[0];
+        const formData = new FormData();
+        formData.append("image", file);
+        await axios.post("http://localhost:3002/uploadImage", formData);
+        console.log("Upload Successes");
+        const imageByName = await axios.get(
+          "http://localhost:3002/getImageByName",
+          {
+            data: { name: file.name },
+            params: { name: file.name },
+          },
+        );
+        const urlOfImage = imageByName.data;
+        setIsLoading(false);
+        setUploadedFile(urlOfImage);
+      }
+    } catch (error) {
+      console.error("Error uploading file to MinIO:", error);
+    }
+  };
+  console.log("uploadedFile", uploadedFile);
+
+  const handleToChooseImageFromAllImages = (e: any) => {
+    console.log(e.target.outerHTML);
+    if (e.target.outerHTML.includes("<path")) {
+      setUploadedFile(e.target.outerHTML);
+    } else {
+      setUploadedFile(e.target.src);
+    }
+    setIsClickShowMore(false);
+  };
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <Button variant="contained" component="label">
+        Upload Image
+        <input
+          type="file"
+          hidden
+          onChange={(e) => handleOnChane(e)}
+          accept="image/*"
+        />
+      </Button>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          boxSizing: "border-box",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h2
+          style={{
+            minWidth: "7vw",
+          }}
+        >
+          All Images
+        </h2>
+        <StyledIconButton onClick={() => setIsClickShowMore(!isClickShowMore)}>
+          {!isClickShowMore && <CollectionsBookmarkIcon />}
+        </StyledIconButton>
+      </div>
+      {isClickShowMore && (
+        <AllImagesModal
+          handleToChooseImageFromAllImages={handleToChooseImageFromAllImages}
+          imagesUrl={imagesUrl}
+          isClickShowMore={isClickShowMore}
+          setIsClickShowMore={setIsClickShowMore}
+        />
+      )}
+      {isLoading && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <CircularProgress />
+        </div>
+      )}
+      {uploadedFile && (
+        <div
+          style={{
+            width: "22vw",
+            height: "37vh",
+            display: "flex",
+            border: "2px #999 dashed",
+            flexDirection: "column",
+            alignItems: "center",
+            marginTop: "5vh",
+          }}
+        >
+          <h3>Selected Image</h3>
+          {uploadedFile.includes("<svg") || uploadedFile.includes("<path") ? (
+            <div
+              style={{ width: "12vw", height: "10vh", display: "contents" }}
+              dangerouslySetInnerHTML={{ __html: uploadedFile }}
+            ></div>
+          ) : (
+            <img
+              src={uploadedFile}
+              style={{ width: "12vw", height: "22vh" }}
+              alt={"upload-file"}
+            />
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ server/src/server.ts
@@ -0,0 +1,105 @@
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import { BucketItem, BucketStream, Client } from "minio";
+import multer from "multer";
+import { isSvgImage } from "./utils";
+import axios from "axios";
+import "dotenv/config";
+
+const app: Express = express();
+
+const options: cors.CorsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+};
+app.use(cors(options));
+app.use(express.json());
+
+const storage = multer.memoryStorage(); // Store files in memory
+const upload = multer({ storage: storage });
+
+const minioClient = new Client({
+  endPoint: process.env.END_POINT || "",
+  port: Number(process.env.PORT),
+  useSSL: false,
+  accessKey: process.env.ACCESS_KEY || "",
+  secretKey: process.env.SECRET_KEY || "",
+});
+
+app.post("/uploadImage", upload.single("image"), async (req, res: any) => {
+  try {
+    const file = req.file as any;
+    console.log(req.file);
+
+    const responseFromMinio = await minioClient.putObject(
+      "demo",
+      file.originalname,
+      file.buffer,
+      {
+        "Content-Type": file.type,
+      },
+    );
+    console.log("responseFromMinio", responseFromMinio);
+
+    res.sendStatus(200);
+  } catch (e) {
+    throw new Error("Cant upload image");
+  }
+});
+
+app.get("/getImageByName", async (req, res) => {
+  try {
+    const nameOfFile = req.query.name;
+    const imageUrlPromise = minioClient.presignedGetObject(
+      "demo",
+      nameOfFile as string,
+    );
+    const imageUrl = await imageUrlPromise;
+    if (isSvgImage(imageUrl)) {
+      console.log("Is svg file");
+      const response = await axios.get(imageUrl);
+      if (response.status === 200) {
+        const svgData = await response.data;
+        return res.status(200).send(svgData);
+      }
+    }
+    res.status(200).send(imageUrl);
+  } catch (e) {
+    throw new Error(`Cant getImageByName ${e}`);
+  }
+});
+
+app.get("/getImages", async (req: Request, res: Response) => {
+  try {
+    const objects: BucketStream<BucketItem> = minioClient.listObjects("demo");
+    const objectNames: string[] = [];
+
+    for await (const object of objects) {
+      objectNames.push(object.name);
+    }
+
+    let imageUrls = await Promise.all(
+      objectNames.map(async (objectName) => {
+        return await minioClient.presignedGetObject("demo", objectName);
+      }),
+    );
+    const ImagesWithSvgFileFormat = imageUrls.map(async (url) => {
+      if (url.includes(".svg")) {
+        const response = await axios.get(url);
+        if (response.status === 200) {
+          const svgData: string = await response.data;
+          return svgData;
+        }
+      }
+      return url;
+    });
+    res.status(200).send(await Promise.all(ImagesWithSvgFileFormat));
+  } catch (error) {
+    console.error("Error getting image URLs:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(3002, async () => {
+  console.log("Server is running on port 3002...");
+});
